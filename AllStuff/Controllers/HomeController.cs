40fabe05aab379993e510fe42b8e24c31b30f147
NameOfFool <ivan.pauk2008@gmail.com>
2023-12-25 13:50:30 +0300

    remove modal

--- AllStuff/Controllers/HomeController.cs
+++ AllStuff/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace AllStuff.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
